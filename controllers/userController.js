a18caff1f801179f66c49685620b51ac31e97093
beefpork <dks81541@gmail.com>
2020-05-18 16:40:18 +0900

    Modified

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-export const join = (req, res) => res.render("loin",{pageTitle: "Join"})
+export const join = (req, res) => res.render("join",{pageTitle: "Join"})
 export const login = (req, res) => res.render("login",{pageTitle: "Login"})
 export const logout = (req, res) => res.render("logout",{pageTitle: "Logout"})
 export const users = (req, res) => res.render("users",{pageTitle: "Users"})
